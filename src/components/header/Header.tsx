9cce6ab4eda8a6e09bcd278703e5d88639a9bb24
DRSJir <jairdelreal@gmail.com>
2026-04-04 14:06:06 -0600

    arreglar el fliker del header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContext";
 import HeaderMobile from "./HeaderMobile";
 import HeaderDesktop from "./HeaderDesktop";
 
 export default function Header() {
     const { totalItems } = useCart();
-    const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-        const checkMobile = () => setIsMobile(window.innerWidth < 1024);
-        checkMobile();
-        window.addEventListener("resize", checkMobile);
-        return () => window.removeEventListener("resize", checkMobile);
-    }, []);
+    return (
+        <>
+            <div className="block md:hidden">
+                <HeaderMobile totalItems={totalItems} />
+            </div>
 
-    if (isMobile) {
-        return <HeaderMobile totalItems={totalItems} />;
-    }
-
-    return <HeaderDesktop totalItems={totalItems} />;
+            <div className="hidden md:block">
+                <HeaderDesktop totalItems={totalItems} />
+            </div>
+        </>
+    );
 }
